301bd52051b04c578fcbece860f3ffbe3eafa1da
kmaterna <kathrynmaterna@gmail.com>
2020-06-22 18:18:35 -0700

    data input pipeline for multitemporal inversion

---
+++ Code/MultiTemporalInversion/drive_slippy_multitemporal.py
@@ -0,0 +1,33 @@
+# June 2020
+# Run Slippy with multiple time intervals in the input 
+# (A really big G matrix)
+
+
+import numpy as np 
+import sys
+import json
+
+
+
+def welcome_and_parse(argv):
+	print("Welcome to the MULTITEMPORAL INVERSION.");
+	if len(argv)<3:
+		print("Error! Please provide the name of a config json. Exiting. "); sys.exit(0);
+	else:
+		fault_config = argv[1];
+		data_config = argv[2];
+	fault_config_file = open(fault_config,'r');
+	config1 = json.load(fault_config_file);
+	data_config_file = open(data_config,'r');
+	config2 = json.load(data_config_file);
+	returnval = {**config1, **config2};
+	return returnval;
+
+
+if __name__=="__main__":
+	config=welcome_and_parse(sys.argv);
+	print(config);
+	# FROM CONFIG: 
+	# READ IN DATA
+	# BUILD G
+	# INVERT
