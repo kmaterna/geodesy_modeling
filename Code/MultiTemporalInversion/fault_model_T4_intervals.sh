b7f5732d0dc07d47c21e4f53e4e9ec91a4d55145
kmaterna <kathrynmaterna@gmail.com>
2020-09-25 09:02:48 -0700

    initial 3d fault visualizer code

--- Code/MultiTemporalInversion/fault_model_T4_intervals.sh
+++ Code/MultiTemporalInversion/fault_model_T4_intervals.sh
@@ -17,40 +17,54 @@ range="-115.62/-115.46/32.97/33.08"
 proj="M2.7i"
 output=$outdir/$title"_ABCDEF.ps"
 field_file="../Mapping_Data/Fields_Boundaries.txt"
+seismicity_T4A="../../Misc_Geophysics_Exps/QTM_exploring/Steps/depth_0_12_32.9_33.1_20120930_20130915/Brawley_QTM.txt"
+seismicity_T4B="../../Misc_Geophysics_Exps/QTM_exploring/Steps/depth_0_12_32.9_33.1_20130915_20140610/Brawley_QTM.txt"
+seismicity_T4C="../../Misc_Geophysics_Exps/QTM_exploring/Steps/depth_0_12_32.9_33.1_20140610_20141015/Brawley_QTM.txt"
+seismicity_T4D="../../Misc_Geophysics_Exps/QTM_exploring/Steps/depth_0_12_32.9_33.1_20141015_20151130/Brawley_QTM.txt"
+seismicity_T4E="../../Misc_Geophysics_Exps/QTM_exploring/Steps/depth_0_12_32.9_33.1_20151130_20161030/Brawley_QTM.txt"
+seismicity_T4F="../../Misc_Geophysics_Exps/QTM_exploring/Steps/depth_0_12_32.9_33.1_20161030_20171101/Brawley_QTM.txt"
 
 gmt makecpt -T$scale_low/$scale_high/$scale_int -Cpolar -D > mycpt.cpt
+gmt makecpt -T0/12/0.1 -Cjet > depth.cpt
 
 # Plotting the slip distribution
 gmt pscoast -R$range -J$proj -Wthin,black -Di -N1 -N2 -B0.2WesN -Y4.5i -Gwhite -Slightgray -K > $output
-echo "-115.59 33.07 T4-A" | gmt pstext -F+f18p,Helvetica-bold -R -J -K -O >> $output
 gmt psxy $slip_file1 -R -J -Wthinnest,gray -Cmycpt.cpt -L -K -O >> $output
 gmt psxy $field_file -R -J$proj -Wthin,indianred -O -K >> $output
+awk '{ print $2, $3, $4, $5*0.04}' $seismicity_T4A | gmt psxy -R$southrange -J$proj -Sc -Cdepth.cpt -K -O >> $output
+echo "-115.59 33.07 T4-A" | gmt pstext -F+f22p,Helvetica-bold -R -J -K -O >> $output
 
 gmt pscoast -R$range -J$proj -Wthin,black -Di -N1 -N2 -B0.2wesn -X3.0i -Gwhite -Slightgray -K -O >> $output
 gmt psxy $slip_file2 -R -J -Wthinnest,gray -Cmycpt.cpt -L -K -O >> $output
-echo "-115.59 33.07 T4-B" | gmt pstext -F+f18p,Helvetica-bold -R -J -K -O >> $output
 gmt psxy $field_file -R -J$proj -Wthin,indianred -O -K >> $output
+awk '{ print $2, $3, $4, $5*0.04}' $seismicity_T4B | gmt psxy -R$southrange -J$proj -Sc -Cdepth.cpt -K -O >> $output
+echo "-115.59 33.07 T4-B" | gmt pstext -F+f22p,Helvetica-bold -R -J -K -O >> $output
 
 gmt pscoast -R$range -J$proj -Wthin,black -Di -N1 -N2 -B0.2wesN -X3.0i -Gwhite -Slightgray -K -O >> $output
-echo "-115.59 33.07 T4-C" | gmt pstext -F+f18p,Helvetica-bold -R -J -K -O >> $output
 gmt psxy $slip_file3 -R -J -Wthinnest,gray -Cmycpt.cpt -L -K -O >> $output
 gmt psxy $field_file -R -J$proj -Wthin,indianred -O -K >> $output
+awk '{ print $2, $3, $4, $5*0.04}' $seismicity_T4C | gmt psxy -R$southrange -J$proj -Sc -Cdepth.cpt -K -O >> $output
+echo "-115.59 33.07 T4-C" | gmt pstext -F+f22p,Helvetica-bold -R -J -K -O >> $output
 
 gmt pscoast -R$range -J$proj -Wthin,black -Di -N1 -N2 -B0.2WeSn -X-6.0i -Y-2.5i -Gwhite -Slightgray -K -O >> $output
-echo "-115.59 33.07 T4-D" | gmt pstext -F+f18p,Helvetica-bold -R -J -K -O >> $output
 gmt psxy $slip_file4 -R -J -Wthinnest,gray -Cmycpt.cpt -L -K -O >> $output
 gmt psxy $field_file -R -J$proj -Wthin,indianred -O -K >> $output
+awk '{ print $2, $3, $4, $5*0.04}' $seismicity_T4D | gmt psxy -R$southrange -J$proj -Sc -Cdepth.cpt -K -O >> $output
+echo "-115.59 33.07 T4-D" | gmt pstext -F+f22p,Helvetica-bold -R -J -K -O >> $output
 
 gmt pscoast -R$range -J$proj -Wthin,black -Di -N1 -N2 -B0.2weSn -X3.0i -Gwhite -Slightgray -K -O >> $output
-echo "-115.59 33.07 T4-E" | gmt pstext -F+f18p,Helvetica-bold -R -J -K -O >> $output
 gmt psxy $slip_file5 -R -J -Wthinnest,gray -Cmycpt.cpt -L -K -O >> $output
 gmt psxy $field_file -R -J$proj -Wthin,indianred -O -K >> $output
+awk '{ print $2, $3, $4, $5*0.04}' $seismicity_T4E | gmt psxy -R$southrange -J$proj -Sc -Cdepth.cpt -K -O >> $output
+echo "-115.59 33.07 T4-E" | gmt pstext -F+f22p,Helvetica-bold -R -J -K -O >> $output
 
 gmt pscoast -R$range -J$proj -Wthin,black -Di -N1 -N2 -B0.2weSn -X3.0i -Gwhite -Slightgray -K -O >> $output
-echo "-115.59 33.07 T4-F" | gmt pstext -F+f18p,Helvetica-bold -R -J -K -O >> $output
 gmt psxy $slip_file6 -R -J -Wthinnest,gray -Cmycpt.cpt -L -K -O >> $output
 gmt psxy $field_file -R -J$proj -Wthin,indianred -O -K >> $output
+awk '{ print $2, $3, $4, $5*0.04}' $seismicity_T4F | gmt psxy -R$southrange -J$proj -Sc -Cdepth.cpt -K -O >> $output
+echo "-115.59 33.07 T4-F" | gmt pstext -F+f22p,Helvetica-bold -R -J -K -O >> $output
 
-gmt psscale -Cmycpt.cpt -Dx-4c/-0.9c+w12c/0.5c+jTC+h -Bxaf -By+l"slip(m)" -K -O >> $output
+gmt psscale -Cmycpt.cpt -Dx0c/-0.9c+w8c/0.5c+jTC+h -Bxaf -By+l"Reverse slip(m)" -K -O >> $output
+gmt psscale -Cdepth.cpt -Dx-11c/-0.9c+w7c/0.5c+jTC+h -Bxaf -By+l"Depth (km)" -O >> $output
 
 # open $output
---
+++ Code/MultiTemporalInversion/visualize_3d_fault.py
@@ -0,0 +1,62 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matplotlib.cm as cm
+import matplotlib
+import slippy.io
+import slippy.xyz2geo
+import slippy.patch
+
+
+def input_slippy_patches(slip_file):
+    inputs = slippy.io.read_slip_data(slip_file);
+    patch_pos_geo = inputs[0]  # 3-column pos
+    bm = slippy.xyz2geo.create_default_basemap(patch_pos_geo[:, 0], patch_pos_geo[:, 1], resolution='i')
+    pos_cart = slippy.xyz2geo.geodetic_to_cartesian(patch_pos_geo, bm)
+    strike = inputs[1]
+    dip = inputs[2]
+    length = inputs[3]
+    width = inputs[4]
+    slip = inputs[5]
+    patches = [slippy.patch.Patch(p, l, w, s, d) for p, l, w, s, d in zip(pos_cart, length, width, strike, dip)]
+    return patches, slip;
+
+
+def plot_3d_patches(patches, slip):
+    # The function that makes a 3D plot for exploring.
+    polys = []
+    for p in patches:
+        vert = p.patch_to_user([[0.0, 0.0, 0.0],
+                                [1.0, 0.0, 0.0],
+                                [1.0, 1.0, 0.0],
+                                [0.0, 1.0, 0.0]])
+        polys += [vert];  # get the 3D vertex positions in meters
+
+    # Create a 3D patch collection
+    pc = Poly3DCollection(polys, edgecolor='black', zorder=0, alpha=1, cmap='viridis')
+    total_slip = np.array(np.sqrt(np.add(np.square(slip[:, 0]), np.square(slip[:, 1]))));
+    pc.set_array(total_slip)
+
+    fig = plt.figure(dpi=200);
+    ax = fig.add_subplot(111, projection='3d')
+    ax.add_collection(pc)
+    ax.set_xlim([-5000, 5000])
+    ax.set_ylim([-5000, 5000])
+    ax.set_zlim([-8000, 0])
+    ax.set_xlabel('Easting (m)');
+    ax.set_ylabel('Northing (m)');
+    ax.set_zlabel('Depth (m)');
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=0.3)
+    cbar = fig.colorbar(cm.ScalarMappable(norm=norm, cmap='viridis'), ax=ax)
+    cbar.set_label('Total Slip (m)');
+
+    plt.show()
+    return;
+
+
+if __name__ == "__main__":
+    slip_output_file = sys.argv[1];
+    patches, slip = input_slippy_patches(slip_output_file);
+    plot_3d_patches(patches, slip);
