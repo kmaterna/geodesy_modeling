1d52ca25e5a25a06c590c73054734d0815b2f1e0
kmaterna <kathrynmaterna@gmail.com>
2025-04-01 12:27:42 -0600

    adding new plot for the multiplication of two stress tensor heat maps

--- geodesy_modeling/datatypes/stress_tensor/plotting.py
+++ geodesy_modeling/datatypes/stress_tensor/plotting.py
@@ -4,21 +4,23 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 
-def heatmap_plot(df, plotname, plottitle=''):
+def heatmap_plot(df, plotname, plottitle='', stresstype='shear'):
     """
     Draw a heat map of a stress component's positive and negative values over all geometries.
 
     :param df: a pandas dataframe that contains columns=['strike', 'dip', 'rake', 'shear', 'normal', 'coulomb']
     :param plotname: string to name the figure
     :param plottitle: string to put as title of the figure
+    :param stresstype: string, 'shear', 'normal', or 'coulomb'
     :return:
     """
-    # Step 2: Make plot of shear stress from this stress tensor
+    # Step 2: Make plot of stress component from this stress tensor
     fig = plt.figure(figsize=(12, 10))
     gs = gridspec.GridSpec(5, 2, figure=fig, wspace=0.05, hspace=0.1)  # Adjust spacing
     fig.suptitle(plottitle, fontsize=18, y=0.92)
     dip = -10
-    levels = np.arange(-8.6, 8.6, 0.1)
+    levels = np.arange(-18.6, 18.6, 0.1)
+    cf = []
     for i in range(5):
         for j in range(2):
             dip += 10
@@ -26,9 +28,9 @@ def heatmap_plot(df, plotname, plottitle=''):
 
             # Create tricontour plots
             cf = ax.tricontourf(df[df['dip'] == dip]['strike'], df[df['dip'] == dip]['rake'],
-                                df[df['dip'] == dip]['shear'], levels=levels, cmap='magma')
+                                df[df['dip'] == dip][stresstype], levels=levels, cmap='magma')
 
-            ax.text(5, 120, f"Dip={dip}")
+            ax.text(5, 120, f"Dip={dip}", backgroundcolor='0.75')
             if j == 1:
                 ax.set_yticklabels([])
             else:
@@ -39,7 +41,61 @@ def heatmap_plot(df, plotname, plottitle=''):
                 ax.set_xlabel('Strike')
 
     # Add a single colorbar on the right
-    cbar_ax = fig.add_axes([0.92, 0.2, 0.02, 0.6])  # [left, bottom, width, height]
-    fig.colorbar(cf, cax=cbar_ax, label="$\mu = 0.1$ Shear Stress (kPa)")
+    cbar_ax = fig.add_axes((0.92, 0.2, 0.02, 0.6))  # [left, bottom, width, height]
+    fig.colorbar(cf, cax=cbar_ax, label="$\mu = 0.1$ "+stresstype+" stress (kPa)")
     plt.savefig(plotname, dpi=300, bbox_inches='tight')
+    plt.close()
+    return
+
+
+def two_heatmaps(df1, df2, plotname, plottitle='', stresstype='shear', nextype='shear_nex'):
+    """
+    Draw a heat map of a stress component's positive and negative values over all geometries.
+
+    :param df1: a pandas dataframe that contains columns=['strike', 'dip', 'rake', 'shear_nex', 'coulomb_nex']
+    :param df2: a pandas dataframe that contains columns=['strike', 'dip', 'rake', 'shear', 'normal', 'coulomb']
+    :param plotname: string to name the figure
+    :param plottitle: string to put as title of the figure
+    :param stresstype: string, 'shear', 'normal', or 'coulomb'
+    :return:
+    """
+    # Step 2: Make plot of stress component from this stress tensor
+    fig = plt.figure(figsize=(12, 10))
+    gs = gridspec.GridSpec(5, 2, figure=fig, wspace=0.05, hspace=0.1)  # Adjust spacing
+    fig.suptitle(plottitle, fontsize=18, y=0.92)
+    dip = -10
+    levels = np.arange(0, 1.0, 0.01)
+    cf = []
+    for i in range(5):
+        for j in range(2):
+            dip += 10
+            ax = fig.add_subplot(gs[i, j])
+
+            x_values1 = df1[df1['dip'] == dip]['strike']
+            y_values1 = df1[df1['dip'] == dip]['rake']
+            z_values1 = df1[df1['dip'] == dip][nextype]
+            z_values2 = df2[df2['dip'] == dip][stresstype]
+
+            z_values1[z_values1 < 0] = 0
+            z_values2[z_values2 > 0] = 0
+            z_values_total = np.multiply(z_values1, z_values2)  # Product positive when both conditions are met
+
+            # Create tricontour plots
+            cf = ax.tricontourf(x_values1, y_values1, z_values_total, levels=levels, cmap='magma')
+
+            ax.text(5, 120, f"Dip={dip}", backgroundcolor='0.75')
+            if j == 1:
+                ax.set_yticklabels([])
+            else:
+                ax.set_ylabel('Rake')
+            if i != 4:
+                ax.set_xticklabels([])
+            else:
+                ax.set_xlabel('Strike')
+
+    # Add a single colorbar on the right
+    cbar_ax = fig.add_axes((0.92, 0.2, 0.02, 0.6))  # [left, bottom, width, height]
+    fig.colorbar(cf, cax=cbar_ax, label="$\mu = 0.1$ " + stresstype + " stress (kPa)")
+    plt.savefig(plotname, dpi=300, bbox_inches='tight')
+    plt.close()
     return
